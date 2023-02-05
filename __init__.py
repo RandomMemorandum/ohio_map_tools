88c440de283995a4e7bc6d40ca5cc1cde62f6c97
Jonathan <jlehman@ocjvc.org>
2023-02-05 15:23:42 -0500

    refines ohio county map with coloring by region

--- __init__.py
+++ __init__.py
@@ -1,3 +1,13 @@
+"""
+
+
+    salmon       #ea7e7e
+    lightblue    #5588ff
+    yellow       #f7dd78
+    burgundy     #913945
+    fuschia      #be29ec
+
+"""
 
 import json
 from pathlib import Path
@@ -13,41 +23,24 @@ class OhioMap:
     def __init__(self):
 
 
-        self.config = self.load_config()
+        self.load_matplotlib_config()
 
         self.odot_county_geojson = None
         self.load_odot_county_geojson()
 
-        self.county_names = None
-        self.load_county_names()
+        self.county_ref = None
+        self.load_county_fips_ocvjc_region_ref()
+
+        self.color_map = None
+        self.load_ocvjc_color_map()
 
 
-    def load_config(self):
+
+    def load_matplotlib_config(self):
         self.FONT_SIZE = 4
         self.DOTS_PER_INCH = 1000
         self.FORMAT = "png"
         self.BBOX_INCHES = "tight"
-
-
-    def random_color(self):
-        """
-
-        salmon       #ea7e7e
-        lightblue    #5588ff
-        yellow       #f7dd78
-        burgundy     #913945
-        fuschia      #be29ec
-        """
-
-        colors = [
-            "#ea7e7e",
-            "#5588ff",
-            "#f7dd78",
-            "#913945",
-            "#be29ec"
-        ]
-
-        return random.choice(colors)
 
 
     def load_json_file(self, file_path):
@@ -62,21 +55,29 @@ class OhioMap:
         self.odot_county_geojson = self.load_json_file(f)
 
 
-    def load_county_names(self):
-        f = Path(Path(__file__).parent, "ohio_counties_fips_code.json")
+    def load_county_fips_ocvjc_region_ref(self):
+        f = Path(Path(__file__).parent, "county_fips_ocvjc_region_ref.json")
         county_name_arr = self.load_json_file(f)
 
         county_name_obj = {}
 
         for obj in county_name_arr:
             code = obj["fips_code"]
-            county_name = obj["county"]
-            county_name_obj[code] = county_name
+            county_name_obj[code] = obj
 
-        self.county_names = county_name_obj
+        self.county_ref = county_name_obj
 
 
-    def draw_plot(self):
+    def load_ocvjc_color_map(self):
+        f = Path(Path(__file__).parent, "ocvjc_region_color_map.json")
+        arr = self.load_json_file(f)
+        output_obj = {}
+        for obj in arr:
+            output_obj[obj["region"]] = obj["hex_color"]
+        self.color_map = output_obj
+
+
+    def draw_ohio_county_plot(self):
 
         plt.rcParams.update({'font.size': self.FONT_SIZE})
 
@@ -85,12 +86,16 @@ class OhioMap:
 
         for feature in self.odot_county_geojson["features"]:
 
-            color = self.random_color()
-
             county_fips = feature["properties"]["FIPS_COUNT"]
 
-            county_name = self.county_names[county_fips]
+            # County Name for Label
+            county_name = self.county_ref[county_fips]["county_name"]
             county_name = "\n".join(county_name.split())
+
+            # Color set by region as configured in 
+            ocvjc_region = self.county_ref[county_fips]["region"]
+            color = self.color_map[ocvjc_region]
+
 
             lat_north = feature["properties"]["LAT_NORTH_"]
             lat_south = feature["properties"]["LAT_SOUTH_"]
@@ -122,3 +127,8 @@ class OhioMap:
                     dpi=self.DOTS_PER_INCH,
                     format=self.FORMAT,
                     bbox_inches=self.BBOX_INCHES)
+
+
+
+
+
